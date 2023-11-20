a1089b2d4f7b1e10f59e3e8d5f1099ee60c9b754
Wickhye <jihyesim170@gmail.com>
2023-11-20 11:09:03 +0900

    chore: 도시 선택 검색으로 바꿈

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import Head from "next/head";
+import Select from "react-select";
 
 export default function Home() {
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const handleCityChange = (selectedOption: any) => {
+    setSelectedCity(selectedOption.value);
+  };
+
+  const cityOptions = [
+    { value: "City 1", label: "City 1" },
+    { value: "City 2", label: "City 2" },
+    { value: "City 3", label: "City 3" },
+  ];
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -10,24 +24,29 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <span className="text-blue-600">Weather Data Visualization</span>
+          <span className="text-blue-600">날씨 데이터 시각화</span>에 오신 것을
+          환영합니다
         </h1>
 
-        <p className="mt-3 text-2xl">Select a city to view its weather data:</p>
+        <p className="mt-3 text-2xl">날씨 데이터를 보려면 도시를 선택하세요.</p>
 
         <div className="flex items-center justify-center mt-4">
-          <select className="border border-blue-300 p-2 rounded-md">
-            <option>Select a city</option>
-            <option>City 1</option>
-            <option>City 2</option>
-            <option>City 3</option>
-          </select>
+          <Select
+            options={cityOptions}
+            onChange={handleCityChange}
+            placeholder="Select a city"
+          />
         </div>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           <div className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
-            <h3 className="text-2xl font-bold">Graph will be displayed here</h3>
+            {selectedCity ? (
+              <h3 className="text-2xl font-bold">
+                {selectedCity}의 날씨 데이터가 이 곳으로 들어올 겁니다.
+              </h3>
+            ) : (
+              <h3 className="text-2xl font-bold">도시를 선택해 주세요</h3>
+            )}
           </div>
         </div>
       </main>
